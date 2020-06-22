2dbca7706b59fda6e92e51930b5798612dd690dc
Leandro <limamonteiro2010@hotmail.com>
2020-06-22 15:10:05 -0300

    a

---
+++ app.py
@@ -0,0 +1,13 @@
+from flask import Flask
+from flask import render_template
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    title = {'titulo': 'Página principal'}
+    return render_template('index.html', **title)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
